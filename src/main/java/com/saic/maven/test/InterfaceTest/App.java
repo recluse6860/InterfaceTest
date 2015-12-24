3c383f24c290007754f746ce38c11388ffc944cc
recluse6860 <recluse6860_cn@hotmail.com>
2015-12-24 15:49:41 +0800

    first commit

---
+++ src/main/java/com/saic/maven/test/InterfaceTest/App.java
@@ -0,0 +1,13 @@
+package com.saic.maven.test.InterfaceTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
