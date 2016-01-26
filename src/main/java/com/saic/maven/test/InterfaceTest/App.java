fa4f052c10a42a5cf7686a39e981cd819995f0ce
shenliang <shenliang@chexiang.com>
2016-01-26 08:55:20 +0800

    add httpclient and venus adaptor

--- src/main/java/com/saic/maven/test/InterfaceTest/App.java
+++ src/main/java/com/saic/maven/test/InterfaceTest/App.java
@@ -1,13 +1,25 @@
 package com.saic.maven.test.InterfaceTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	/* Get actual class name to be printed on */
+	static Logger logger = LogManager.getLogger(App.class.getName());
+	
+    public static void main( String[] args ) throws Exception 
     {
+    	Logger logger = LogManager.getLogger(App.class);
         System.out.println( "Hello World!" );
+        logger.debug("Hello World!");
+        logger.info("Info");
+        logger.warn("warning!");
+        logger.error("error");
     }
 }
---
+++ src/test/java/com/saic/maven/test/InterfaceTest/VenusAdaptor.java
@@ -0,0 +1,5 @@
+package com.saic.maven.test.InterfaceTest;
+
+public class VenusAdaptor {
+
+}
